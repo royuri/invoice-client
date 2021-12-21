76d7bfd63b23c879a4d15dd44c323615ed5fb3c8
royuri <94353032+royuri@users.noreply.github.com>
2021-12-21 08:28:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import reducers from './reducers/'
 
-
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
